f337694002931a8b3f0fea273163c56dec2f2f5b
Tom Tang <tly1980@gmail.com>
2019-07-31 15:53:14 +1000

    more tests are better

--- producer/file.go
+++ producer/file.go
@@ -2,10 +2,11 @@ package producer
 
 import (
   "bufio"
+  "encoding/csv"
+  "io"
   "net/url"
   "path"
   "strings"
-  "io"
 
   log "github.com/sirupsen/logrus"
   "http2gcs/task"
@@ -45,42 +46,36 @@ func FileListToDirProducer(reader io.Reader, baseUri string, tasks chan *task.Ta
   return nil
 }
 
-/*
-func CSVFileToFileProducer(fPath string, tasks chan *task.Task) error {
-  uParse, err := url.Parse(baseUri)
 
-  if err != nil {
-    return err
-  }
+func CSVFileToFileProducer(reader io.Reader,  tasks chan *task.Task) error {
+  csvReader := csv.NewReader(reader)
+  for {
+    record, err := csvReader.Read()
 
-  destScheme := uParse.Scheme
-  destBkt := uParse.Host
-  destBase := strings.Trim(uParse.Path, "/")
-  file, err := os.Open(fPath)
-  if err != nil {
-    log.Error(err)
-    return err
-  }
-  defer file.Close()
+    switch err {
+      case io.EOF:
+        return nil
+      case nil:
+        src := record[0]
+        dest := record[1]
+        uParse, err1 := url.Parse(dest)
+        if err1 != nil {
+          return err1
+        }
+        destScheme := uParse.Scheme
+        destBkt := uParse.Host
+        destBase := strings.TrimLeft(uParse.Path, "/")
+        var destKey string
+        if (strings.HasSuffix(destBase, "/")) {
+          destKey = path.Join(destBase, path.Base(src))
+        } else {
+          destKey = destBase
+        }
 
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan() {
-    src := scanner.Text()
-    fName := path.Base(src)
-    t := &task.Task{
-        src,
-        destScheme,
-        destBkt,
-        path.Join(destBase, fName),
+        tasks <- &task.Task{ src, destScheme, destBkt, destKey}
+      default:
+        return err
     }
-    tasks <- t
   }
-
-  if err = scanner.Err(); err != nil {
-    log.Fatal(err)
-    return err
-  }
-
   return nil
 }
-*/
--- tests/producer_test.go
+++ tests/producer_test.go
@@ -8,6 +8,8 @@ import (
 	// "github.com/stretchr/testify/assert"
 	"http2gcs/task"
 	"http2gcs/producer"
+
+
 )
 
 func TestFileListToDirProducer(t *testing.T) {
@@ -21,3 +23,26 @@ func TestFileListToDirProducer(t *testing.T) {
 	cupaloy.SnapshotT(t, allTasks)
 }
 
+/*
+func TestFileListToDirProducer(t *testing.T) {
+	ch := make(chan *task.Task)
+	file, _ := os.Open("fixtures/task1.csv")
+	defer file.Close()
+
+	go producer.FileListToDirProducer(file, "gs://ut/a/b/c", ch)
+	l1, l2, l3, l4 := <-ch, <-ch, <-ch, <-ch
+	allTasks := []*task.Task {l1, l2, l3, l4}
+	cupaloy.SnapshotT(t, allTasks)
+}
+*/
+
+func TestCSVFileToFileProducer(t *testing.T) {
+	ch := make(chan *task.Task)
+	file, _ := os.Open("fixtures/task1.csv")
+	defer file.Close()
+
+	go producer.CSVFileToFileProducer(file, ch)
+	l1, l2, l3 := <-ch, <-ch, <-ch
+	allTasks := []*task.Task {l1, l2, l3}
+	cupaloy.SnapshotT(t, allTasks)
+}
